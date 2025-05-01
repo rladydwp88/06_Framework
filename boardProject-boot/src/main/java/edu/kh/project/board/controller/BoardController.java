2fca40fca67fd0a98ca241124293d1eb87298cbb
rladydwp88 <rladydwp88@gmail.com>
2025-05-01 16:06:54 +0900

    학원

--- boardProject-boot/src/main/java/edu/kh/project/board/controller/BoardController.java
+++ boardProject-boot/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -1,5 +1,7 @@
 package edu.kh.project.board.controller;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,8 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,6 +22,9 @@ import edu.kh.project.board.model.dto.Board;
 import edu.kh.project.board.model.dto.BoardImg;
 import edu.kh.project.board.model.service.BoardService;
 import edu.kh.project.member.model.dto.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -89,7 +97,10 @@ public class BoardController {
 							@PathVariable("boardNo") int boardNo,
 							Model model,
 							@SessionAttribute(value="loginMember", required = false) Member loginMember,
-							RedirectAttributes ra) {
+							RedirectAttributes ra,
+							HttpServletRequest req, // 요청에 담긴 쿠키 얻어오기
+							HttpServletResponse resp // 새로운 쿠키 만들어 
+							) {
 		
 		// 게시글 상세 조회 서비스 호출
 		
@@ -114,6 +125,75 @@ public class BoardController {
 			ra.addFlashAttribute("message", "게시글이 존재하지 않습니다.");
 			
 		} else {
+			
+			/* ----------------- 쿠키를 이용한 조회 수 증가 시작 --------------- */
+			// 비회원 또는 로그인한 회원의 글이 아닌 경우( == 글쓴이를 뺀 다른 사람 )
+			if(loginMember == null || loginMember.getMemberNo() != board.getMemberNo()) {
+				
+				// 요청에 담겨있는 모든 쿠키 얻어오기
+				Cookie[] cookies = req.getCookies();
+				
+				Cookie c = null;
+				
+				for(Cookie temp : cookies) {
+					// 요청에 담긴 쿠키에 "readBoardNo"가 존재할 때
+					if(temp.getName().equals("readBoardNo")) {
+						c = temp;
+						break;
+					}
+				}
+				
+				int result = 0; // 조회 수 증가 결과를 저장할 변수
+				
+				// "readBoardNo"가 쿠키에 없을 때
+				if(c != null) {
+					// 새 쿠키 생성("readBoardNo", [게시글 번호])
+					c = new Cookie("readBoardNo", "[" + boardNo + "]");
+					result = service.updateReadCount(boardNo);
+					
+				} else {
+				// "readBoardNo"가 쿠키에 dlT을 때
+				// "readBoardNo" : [2][30][400]
+					
+				// 현재 게시글을 처음 읽는 경우
+					if(c.getValue().indexOf("[" + boardNo + "]") == -1) {
+						// 해당 글 번호를 쿠키에 누적 + 서비스 호출
+						c.setValue(c.getValue() + "[" + boardNo + "]");
+						result = service.updateReadCount(boardNo);
+					}
+					
+				}
+				
+				// 조회 수 증가 성공 / 조회 성공 시
+				if(result > 0) {
+					// 먼저 조회된 board의 readCount 값을
+					// result 값으로 다시 세팅
+					board.setReadCount(result);
+					
+					// 쿠키 적용 경로 설정
+					c.setPath("/"); // "/" 이하 경로 요청 시 쿠키 서버로 전달
+					
+					// 쿠키 수명 지정
+					// 현재 시간을 얻어오기
+					LocalDateTime now = LocalDateTime.now();
+					// 다음날 자정 지정
+					LocalDateTime nextDayMidnight = now.plusDays(1)
+														.withHour(0)
+														.withMinute(0)
+														.withSecond(0)
+														.withNano(0);
+					
+					// 현재 시간부터 다음날 자정까지 남은 시간 계산 (조단위)
+					long seconds = Duration.between(now, nextDayMidnight).getSeconds();
+					
+					// 쿠키 수명 설정
+					c.setMaxAge((int)seconds);
+					resp.addCookie(c); // 응답 객체를 이용해서 클라이언트에게 쿠키 전달
+				}
+			}
+			
+			/* ----------------- 쿠키를 이용한 조회 수 증가 끝 ----------------- */
+			
 			// 조회 결과가 있는 경우
 			path = "board/boardDetail"; // boardDetail.html로 forword
 			
@@ -138,5 +218,15 @@ public class BoardController {
 		}
 		
 		return path;
+	}
+	
+	/** 게시글 좋아요 체크/해제
+	 * @param map
+	 * @return
+	 */
+	@ResponseBody
+	@PostMapping("like") // / board/like (POST)
+	public int boardLike(@RequestBody Map<String, Integer> map) {
+		return service.boardLike(map);
 	}
 }
